65d300ff48bc5a2695920e57c9d8a6154ace9c76
said daikach <saiddaikach6@gmail.com>
2021-12-14 12:38:51 +0100

    Add project files.

---
+++ KarateKidSchool.Resoursce/Class1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KarateKidSchool.Resoursce
+{
+    public class Class1
+    {
+    }
+}
